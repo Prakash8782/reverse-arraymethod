21eaa74de528113bbfcad1504421337bfdb9bc31
Prakash8782 <prakash8782@gmail.com>
2022-09-19 15:46:55 +0530

    reverse

---
+++ reverse/main.js
@@ -0,0 +1,7 @@
+let colors=["Red","Blue","Green","Yellow","Pink"];
+console.log("colors : ", colors);
+console.log("length of an array : "+colors.length);
+console.log("method : array.reverse()");
+colors.reverse();
+console.log("change in array :", colors);
+console.log("return : length of array after reverse(); is "+colors.length);
